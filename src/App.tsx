c1d00ad077553e60df3182d85745b6bfd6d2e4f1
Yoshida010 <kamui0923@ezweb.ne.jp>
2022-11-27 11:57:01 +0900

    追加 App.tsにpractices配下のコンポーネントを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 /** @format */
-
-import { useState } from "react";
-import "./App.css";
+import { Practice01 } from "./practices/Practice01";
+import { Practice02 } from "./practices/Practice02";
+import { Practice03 } from "./practices/Practice03";
+import { Practice04 } from "./practices/Practice04";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<div className="App">
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+			<Practice01 />
+			<Practice02 />
+			<Practice03 />
+			<Practice04 />
 		</div>
 	);
 }
